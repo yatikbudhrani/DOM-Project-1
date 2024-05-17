d559e41121eb2c9bf2ee917fdadee6738944bab5
Yatik Budhrani <f20180437g@alumni.bits-pilani.ac.in>
2024-05-17 16:22:55 +0530

    DOM Manipulation

---
+++ index.js
@@ -0,0 +1,10 @@
+var element = document.getElementById("container");
+
+element.style.color = "darkmagenta";
+element.style.backgroundColor = "yellow";
+element.style.margin = "20px";
+element.style.padding = "10px";
+element.style.fontSize = "18px";
+element.style.fontWeight = "bold";
+element.style.height = "200px";
+element.style.width = "300px";
